c02f63a97007886e0980dbc0e094b03382fc32d5
Esteban Cortina <estebannsebastian@gmail.com>
2024-06-08 11:35:31 -0600

    Add(mocha): bodyValitador unit test

--- src/middlewares/bodyValidator.ts
+++ src/middlewares/bodyValidator.ts
@@ -6,7 +6,6 @@ import badRequest from "../messages/error/badRequest.js";
 export default async (req: Request, res: Response, next: NextFunction) => {
     const validatorFilename = getBodyValidatorByRoutePath(req.route.path)
     const module = await import((`../types/validators/${validatorFilename}`))
-
     for (let [key, value] of Object.entries(req.body)) {
         if (!module.default.hasOwnProperty(key) || typeof value !== module.default[key]) {
             return res.status(400).send(badRequest())
