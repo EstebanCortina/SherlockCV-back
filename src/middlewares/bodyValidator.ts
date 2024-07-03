c83296cb4faedf597ca0672064cbc489642a40d5
estebanc <ecortina@devteam.hmhsistemas.com.mx>
2024-07-03 16:54:36 -0600

    Fix: body validator with missing fields, morgan combined format for prod envs & create new user with query params

--- src/middlewares/bodyValidator.ts
+++ src/middlewares/bodyValidator.ts
@@ -6,10 +6,19 @@ import badRequest from "../messages/error/badRequest.js";
 export default async (req: Request, res: Response, next: NextFunction) => {
     const validatorFilename = getBodyValidatorByRoutePath(req.route.path)
     const module = await import((`../types/validators/${validatorFilename}`))
+    const requiredFields = Object.keys(module.default).reverse()
     for (let [key, value] of Object.entries(req.body)) {
         if (!module.default.hasOwnProperty(key) || typeof value !== module.default[key]) {
+            console.log("Bad body request")
+            console.log(req.body)
             return res.status(400).send(badRequest())
         }
+        requiredFields.pop()
+    }
+    if (requiredFields.length) {
+        console.log("Missing required fields in body");
+        console.log(requiredFields)
+        return res.status(400).send(badRequest())
     }
     next()
 }
