929fc4b9c8144b5d0be80a152e80c53564d810ac
estebanc <ecortina@devteam.hmhsistemas.com.mx>
2024-07-05 16:47:14 -0600

    Refactor[#21]: Add required & optional fields in bodyValidator and add string validation

--- src/middlewares/bodyValidator.ts
+++ src/middlewares/bodyValidator.ts
@@ -6,14 +6,28 @@ import badRequest from "../messages/error/badRequest.js";
 export default async (req: Request, res: Response, next: NextFunction) => {
     const validatorFilename = getBodyValidatorByRoutePath(req.route.path)
     const module = await import((`../types/validators/${validatorFilename}`))
-    const requiredFields = Object.keys(module.default).reverse()
+    const requiredFields = Object.keys(module.default.required).reverse()
+    const allFields = {...module.default.required, ...module.default.optional}
+
     for (let [key, value] of Object.entries(req.body)) {
-        if (!module.default.hasOwnProperty(key) || typeof value !== module.default[key]) {
+        if (module.default.required.hasOwnProperty(key)){
+            requiredFields.pop()
+        }
+
+        if (typeof value !== allFields[key]) {
             console.log("Bad body request")
             console.log(req.body)
             return res.status(400).send(badRequest())
         }
-        requiredFields.pop()
+
+        if (typeof value === "string"){
+            if (!allFields[key].length) {
+                console.log("Bad body request")
+                console.log(req.body)
+                return res.status(400).send(badRequest())
+            }
+        }
+
     }
     if (requiredFields.length) {
         console.log("Missing required fields in body");
