8a785e4808a065cb76de2813eb2dbf8b4624068d
estebanc <ecortina@devteam.hmhsistemas.com.mx>
2024-08-27 17:05:49 -0600

    Feat[#38]: Add index & show to CRUD reports

--- src/controllers/reportController.ts
+++ src/controllers/reportController.ts
@@ -1,16 +1,23 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import ReportModel from "../models/ReportModel.js";
 import success from "../messages/success.js";
 
-const reportModel = new ReportModel()
+const reportModel = new ReportModel();
 
 class ReportController {
 
+  /**
+   * Lists all reports associated with the current user.
+   * @param {Request} req - The Express request object, contains `userId` to filter the user's reports.
+   * @param {Response} res - The Express response object.
+   * @returns {Promise<Response>} - Returns an HTTP response with status code 200 and the list of reports.
+   */
   async indexAsync(req: Request, res: Response): Promise<Response> {
     const reports = await (
         reportModel
             .index()
-    ).run()
+            .where("user_id=?")
+    ).run([req.userId]);
     return res.status(200).send(success(
         200,
         "Index Reports",
@@ -18,11 +25,18 @@ class ReportController {
     ));
   }
 
+  /**
+   * Displays the details of a specific report if it belongs to the user and has not been deleted.
+   * @param {Request} req - The Express request object, contains `params.id` and `userId` to identify the report.
+   * @param {Response} res - The Express response object.
+   * @returns {Promise<Response>} - Returns an HTTP response with status code 200 and the report details.
+   */
   async showAsync(req: Request, res: Response): Promise<Response> {
-    const report = await (
+    const report = (await (
         reportModel
-            .find(req.params.id)
-    ).run()
+            .index()
+            .where("id=? AND user_id=? AND deleted_at IS NULL")
+    ).run([req.params.id, req.userId]))[0] ?? {};
     return res.status(200).send(success(
         200,
         "Show Report",
@@ -30,18 +44,25 @@ class ReportController {
     ));
   }
 
-
+  /**
+   * Creates a new report associated with the current user.
+   * @param {Request} req - The Express request object, contains `userId` and `analysis` to create the report.
+   * @param {Response} res - The Express response object.
+   * @returns {Promise<Response>} - Returns an HTTP response with status code 201 and the details of the created report.
+   */
   async createAsync(req: Request, res: Response): Promise<Response> {
     const newReport = await (
         reportModel
             .create({
+              user_id: req.userId,
+              job_position_id: req.body.job_position_id,
               final_analysis: req.analysis
             }, true)
-    ).run()
+    ).run();
 
-    return res.status(200).send(success(
-        200,
-        "New Report",
+    return res.status(201).send(success(
+        201,
+        "New Report Created",
         newReport
     ));
   }
--- src/models/ReportModel.ts
+++ src/models/ReportModel.ts
@@ -1,6 +1,7 @@
 import BaseModel from "../abstract_classes/BaseModel.js";
 
 export default class ReportModel extends BaseModel{
+    user_id?: string;
     final_analysis?: string;
     created_at?: string;
     deleted_at?: string;
